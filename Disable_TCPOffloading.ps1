c47e234984b0f6c2510b9173f72bcac4ead61a9d
robward <robward@gmail.com>
2014-05-21 06:23:57 -0700

    AWS - Turn off TCP Offloading
    Turn off TCP/IP Offloading, LSO and LRO

---
+++ Disable_TCPOffloading.ps1
@@ -0,0 +1,17 @@
+﻿$root = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}'
+$items = Get-ChildItem -Path Registry::$Root -Name
+ 
+Foreach ($item in $items) {
+	if ($item -ne "Properties") {
+		$path = $root + "\" + $item
+		$DriverDesc = Get-ItemProperty -Path Registry::$path | Select-Object -expandproperty DriverDesc
+		if ($DriverDesc -eq "Citrix PV Ethernet Adapter") {
+			Set-ItemProperty -path Registry::$path -Name LROIPv4 -Value 0
+			Set-ItemProperty -path Registry::$path -Name *IPChecksumOffloadIPv4 -Value 0
+			Set-ItemProperty -path Registry::$path -Name *TCPChecksumOffloadIPv4 -Value 0
+			Set-ItemProperty -path Registry::$path -Name *LSOv2IPv4 -Value 0
+		}
+	}
+}
+ 
+Restart-NetAdapter -Name Ethernet
