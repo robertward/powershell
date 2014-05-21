0b72216eac8fa538bb790e64ed6e549bb75c4970
robward <robward@gmail.com>
2014-05-21 06:27:33 -0700

    AWS - SES Test
    Used to test your SES creds

---
+++ SES_Test.ps1
@@ -0,0 +1,19 @@
+﻿$smtpServer = "email-smtp.us-east-1.amazonaws.com"
+$smtpPort = 587
+$username = "ACCESS_KEY"
+$password = "SECRET_KEY"
+$from = "VERIFIED_ADDRESS"
+$to = "EMAIL_ADDRESS"
+$subject = "Test e-mail with PowerShell"
+$body = "This is a test e-mail sending with using PowerShell"
+     
+$smtp = new-object Net.Mail.SmtpClient($smtpServer, $smtpPort)
+$smtp.EnableSsl = $true
+     
+$smtp.Credentials = new-object Net.NetworkCredential($username, $password)
+$msg = new-object Net.Mail.MailMessage
+$msg.From = $from
+$msg.To.Add($to)
+$msg.Subject = $subject
+$msg.Body = $body
+$smtp.Send($msg)
