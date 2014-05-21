2b7b5d9a60ad8148bc070840104125170b0238fd
robward <robward@gmail.com>
2014-05-21 06:22:14 -0700

    AWS - Re-Enable RedHat driver
    Re-enable Red Hat drivers via the registry (After moving the root volume
    to a new instance)

---
+++ reenable_redhat_driver.ps1
@@ -0,0 +1,17 @@
+﻿foreach ($drive in Get-WmiObject Win32_logicaldisk | Where-Object {$_.DriveType -like 3}){
+	if ($drive.DeviceID -ne "C:"){
+		$dir = $drive.DeviceID + "\Windows\System32\config\SYSTEM"
+		reg load HKLM\RECOVERY $dir | Out-Null
+		$regkey = "HKLM:\RECOVERY\ControlSet001\Services\rhelscsi"
+		if (Test-Path $regkey){
+			Set-ItemProperty -Path $regkey -name EnumerateDevices -value 1
+			Write-Host "Redhat Drivers re-enabled"
+			"select disk 1", "offline disk" | diskpart | Out-Null
+			Write-Host "Drive offline"
+		}else{
+			Write-Host "Redhat drivers not installed"
+		}
+		[gc]::collect()
+		reg unload HKLM\RECOVERY | Out-Null
+	}
+}
